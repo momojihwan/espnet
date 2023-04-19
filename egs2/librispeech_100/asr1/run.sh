c8697feb50da4d3dd076b5bd5ea0f7ba7431e64e
momojihwan <momojihwan@gmail.com>
2023-04-19 13:10:50 +0900

    transducer config

--- egs2/librispeech_100/asr1/run.sh
+++ egs2/librispeech_100/asr1/run.sh
@@ -7,20 +7,20 @@ set -o pipefail
 
 train_set="train_clean_100"
 valid_set="dev"
-test_sets="test_clean test_other dev_clean dev_other"
+test_sets="test_clean test_other"
 
-asr_config=conf/train_asr.yaml
-inference_config=conf/decode_asr.yaml
+asr_task="asr_transducer"
+asr_config="conf/tuning/transducer/train_asr_conformer-statelesst-streaming.yaml"
+inference_config="conf/tuning/transducer/decode_asr_transducer.yaml"
 
 ./asr.sh \
     --lang en \
     --ngpu 1 \
     --nj 16 \
-    --gpu_inference true \
-    --inference_nj 2 \
+    --inference_nj 16 \
     --nbpe 5000 \
     --max_wav_duration 30 \
-    --speed_perturb_factors "0.9 1.0 1.1" \
+    --asr_task "${asr_task}" \
     --audio_format "flac.ark" \
     --feats_type raw \
     --use_lm false \
